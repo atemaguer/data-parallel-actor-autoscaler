ecedb09465e43535bca8d66dae67376ad287502a
Atem Aguer <atemsunday@gmail.com>
2022-05-08 20:31:34 -0700

    move autoscaler initialization into coordinator

--- tests/test_autoscaler.py
+++ tests/test_autoscaler.py
@@ -5,25 +5,30 @@ from ray.util.queue import Queue
 
 from dpa_autoscaler.coordinator import MapReduceCoordinator
 from dpa_autoscaler.config import data
-from dpa_autoscaler.executors import map_func, reducer
+from dpa_autoscaler.executors import map_func, reduce_func, reducer
 from dpa_autoscaler.autoscaler import AutoScaler
 
-ray.init(address="auto", ignore_reinit_error=True)
+ray.init(ignore_reinit_error=True)
 
 NUM_MAPPERS = 2
 NUM_REDUCERS = 4
 
 out_queue = Queue()
-red = reducer()
-autoscaler = AutoScaler.options(name="autoscaler").remote()
+reduce_func = reducer()
+
 coord = MapReduceCoordinator.options(name="coordinator").remote(
-    data, NUM_MAPPERS, NUM_REDUCERS, map_func, red, out_queue
+    data, NUM_MAPPERS, NUM_REDUCERS, map_func, reduce_func, out_queue, autoscale=True
 )
 
+ray.get(coord.run.remote())
 
-coord.run.remote()
+# while True:
+#     try:
+#         autoscaler = ray.get_actor("autoscaler")
+#         break
+#     except:
+#         pass
 
 while not ray.get(coord.is_done.remote()):
     print(ray.get(autoscaler.autoscaler_state.remote()))
-    print(out_queue.size())
-    time.sleep(1)
+    time.sleep(5)
