715a73388e66da1006288bd77ce9186ee8f9cffc
amir <arziai@gmail.com>
2022-05-24 12:31:31 -0700

    argparse

--- tests/test_autoscaler.py
+++ tests/test_autoscaler.py
@@ -1,7 +1,7 @@
+import argparse
 import time
 import ray
 import collections
-import sys
 from ray.util.queue import Queue
 
 from dpa_autoscaler.coordinator import MapReduceCoordinator
@@ -9,43 +9,52 @@ from dpa_autoscaler.config import data
 from dpa_autoscaler.executors import map_func, Reducer
 from dpa_autoscaler.autoscaler import AutoScaler
 
-ray.init(ignore_reinit_error=True)
 
-NUM_MAPPERS = 4
-NUM_REDUCERS = 4
-CH_TYPE = "halving"  # the other option is "doubling"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--autoscale", action="store_true")
+    parser.add_argument("--doubling", action="store_true")
+    args = parser.parse_args()
+    autoscale = args.autoscale
+    ch_type = "doubling" if args.double else "halving"
+    print(f"Running with autoscale={autoscale} and consistent hashing type={ch_type}")
 
-out_queue = Queue()
-reduce_func = Reducer()
+    ray.init(ignore_reinit_error=True)
 
-autoscaler = AutoScaler.options(name="autoscaler").remote(
-    num_reducers=NUM_REDUCERS, ch_type=CH_TYPE
-)
+    NUM_MAPPERS = 4
+    NUM_REDUCERS = 4
 
-coord = MapReduceCoordinator.options(name="coordinator").remote(
-    data,
-    NUM_MAPPERS,
-    NUM_REDUCERS,
-    map_func,
-    reduce_func,
-    out_queue,
-    ch_type=CH_TYPE,
-    autoscale=(int(sys.argv[1]) == 0),
-)
+    out_queue = Queue()
+    reduce_func = Reducer()
 
-ray.get(coord.run.remote())
+    autoscaler = AutoScaler.options(name="autoscaler").remote(
+        num_reducers=NUM_REDUCERS, ch_type=ch_type
+    )
 
-while not ray.get(coord.is_done.remote()):
-    time.sleep(5)
+    coord = MapReduceCoordinator.options(name="coordinator").remote(
+        data,
+        NUM_MAPPERS,
+        NUM_REDUCERS,
+        map_func,
+        reduce_func,
+        out_queue,
+        ch_type=ch_type,
+        autoscale=autoscale,
+    )
 
-# print(ray.get(autoscaler.autoscaler_state.remote()))
-print(f"Running time is: {ray.get(coord.running_time.remote())}")
+    ray.get(coord.run.remote())
 
-d = collections.defaultdict(int)
-while not out_queue.empty():
-    out = out_queue.get()
-    print(out)
-    for k, v in out.items():
-        d[k] += v
-print(len(d))
-print(d)
+    while not ray.get(coord.is_done.remote()):
+        time.sleep(5)
+
+    # print(ray.get(autoscaler.autoscaler_state.remote()))
+    print(f"Running time is: {ray.get(coord.running_time.remote())}")
+
+    d = collections.defaultdict(int)
+    while not out_queue.empty():
+        out = out_queue.get()
+        print(out)
+        for k, v in out.items():
+            d[k] += v
+    print(len(d))
+    print(d)
